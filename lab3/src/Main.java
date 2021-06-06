cf42bd01ce448f1e519fe0241688df15e9e70647
naadzya <nadyagrishalevich@gmail.com>
2021-06-06 10:24:44 +0300

    console input for gemstone

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -4,17 +4,140 @@ import src.Stone.Stone;
 import src.Gemstone.Gemstone;
 import src.SemiGemstone.SemiGemstone;
 import src.Jewerly.Jewerly;
-import java.util.Arrays;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
+    public static int menu() {
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose from these options:");
+        System.out.println("1 - Create a Jewerly");
+        System.out.println("2 - Get list of all available gemstones");
+        System.out.println("3 - Get list of all available semi-gemstones");
+        System.out.println("4 - Quit");
+        System.out.print("Your choise: ");
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    public static Gemstone createGemstone() {
+        Scanner inputName = new Scanner(System.in);
+        Scanner inputMass = new Scanner(System.in);
+        Scanner inputCost = new Scanner(System.in);
+        Scanner inputValue = new Scanner(System.in);
+        Scanner inputTransparency = new Scanner(System.in);
+        
+        String name;
+        double mass = 0, cost = 0;
+        int value = 0, transparency = 0;
+
+        System.out.print("Enter gemestone's name: ");
+        name = inputName.next();
+
+        boolean isCorrect = false;
+        while (! isCorrect) {
+            System.out.print("Enter gemestone's mass in carats: ");
+            try {
+                mass = inputMass.nextDouble();
+                isCorrect = true;
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Wrong mass, try again.");
+                inputMass.nextLine();
+                isCorrect = false;
+            }
+        }
+        
+        isCorrect = false;
+        while (! isCorrect) {
+            System.out.print("Enter gemestone's cost in dollars: ");
+            try {
+                cost = inputCost.nextDouble();
+                isCorrect = true;
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Wrong cost, try again.");
+                inputCost.nextLine();
+                isCorrect = false;
+            }
+        }
+
+        isCorrect = false;
+        while (! isCorrect) {
+            System.out.print("Enter gemestone's value, which is an integer between 0 and 10: ");
+            try {
+                value = inputValue.nextInt();
+                isCorrect = true;
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Wrong value, try again.");
+                inputValue.nextLine();
+                isCorrect = false;
+            }
+        }
+       
+        isCorrect = false;
+        while (! isCorrect) {
+            System.out.print("Enter gemestone's transparency, which is an integer which is an integer between 0 and 100: ");
+            try {
+                transparency = inputTransparency.nextInt();
+                isCorrect = true;
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Wrong transparency, try again.");
+                inputTransparency.nextLine();
+                isCorrect = false;
+            }
+        }
+
+        Gemstone result = new Gemstone(name, mass, cost, value, transparency);
+        return result;
+    }
+
     public static void main(String[] args) {
-        Gemstone ruby = new Gemstone("Ruby", 1.1, 2.5, 8, 50);
-        SemiGemstone agate = new SemiGemstone("Agate", 1.1, 2.8, 5, 10);
-        Gemstone diamond = new Gemstone("Diamond", 1.1, 2.8, 10, 90);
-        Gemstone diamond2 = new Gemstone("Diamond", 1.1, 2.8, 10, 70);
-        Jewerly testjw = new Jewerly(new Stone[] {ruby, agate, diamond, diamond2}, "Channel");
-        //testjw.sortStonesByValue();
-        //System.out.println(Arrays.toString(testjw.getStones()));
-        System.out.println(Arrays.toString(testjw.getTransparentStones(40, 100)));
+        int userChoice = menu();
+        while (userChoice != 4) {
+            switch(userChoice) {
+                case 1:
+                    System.out.println(createGemstone().toString());
+                    break;
+                case 2:
+                    System.out.println("All available gemstones: " + 
+                                       "Diamond, " + 
+                                       "Ruby, " + 
+                                       "BlueSapphire, " +
+                                       "Emerald");
+                    break;
+                case 3:
+                    System.out.println("All available semi-gemstones: " + 
+                                       "Alexandrite, " +
+                                       "Agate, " +
+                                       "Amethyst, " +
+                                       "Aquamarine, " +
+                                       "Garnet, " +
+                                       "LapisLazuli, " +
+                                       "Moonstone, " +
+                                       "Opal, " +
+                                       "Pearl, " +
+                                       "Peridot, " +
+                                       "RoseQuartz, " +
+                                       "Spinel, " +
+                                       "Tanzanite, " +
+                                       "Tourmaline, " +
+                                       "Turquoise, " +
+                                       "Zircon"); 
+                    break;
+                default:
+                    System.out.println("Wrong input. Try again");
+            }
+            
+            System.out.println("-------------------------");
+            userChoice = menu();
+        }
+        System.out.println("Bye!");
     }
 }
